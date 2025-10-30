959a30da7f7ea105cbb2965bcf5eb8b47087186d
Nilay Alkesh Vaghela <nilay@Nilays-MacBook-Air.local>
2025-10-30 14:29:07 -0400

    error solv in backend

--- server/supabase.js
+++ server/supabase.js
@@ -1,6 +1,9 @@
+// server/supabase.js
 import { createClient } from "@supabase/supabase-js";
-export const sbAdmin = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE,
-  { auth: { persistSession: false } }
-);
+
+const url = process.env.SUPABASE_URL;
+const anon = process.env.SUPABASE_ANON_KEY;
+const service = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+export const sbAnon = createClient(url, anon, { auth: { persistSession: false } });
+export const sbAdmin = createClient(url, service, { auth: { persistSession: false } });
